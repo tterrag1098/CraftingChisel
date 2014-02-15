9a04d8d28922b4ed3651d51f4984d0f5c10366eb
tterrag1098 <tterrag1098@gmail.com>
2014-02-14 23:04:39 -0500

    Fix right click with block in hand

--- src/main/java/tterrag/craftingChisel/block/OmniCraftingTable.java
+++ src/main/java/tterrag/craftingChisel/block/OmniCraftingTable.java
@@ -101,10 +101,14 @@ public class OmniCraftingTable extends Block
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float headx, float heady, float headz)
 	{
 		if (!world.isRemote && (player.inventory.getCurrentItem() != null && player.inventory.getCurrentItem().getItem() != CraftingChisel.chisel) || player.inventory.getCurrentItem() == null) 
+		{
 			player.openGui(CraftingChisel.instance, 0, world, x, y, z);
-		else if (!world.isRemote) 
-				reset(world, x, y, z);
-		return false;
+		}
+		else if (!world.isRemote)
+		{
+			reset(world, x, y, z);
+		}
+		return true;
 	}
 	
 	private void reset(World world, int x, int y, int z)
