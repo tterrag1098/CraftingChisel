a1b21ce2234bdac9056ddd0b3e3da04589a2e007
tterrag1098 <tterrag1098@gmail.com>
2014-02-15 21:28:34 -0500

    Fix NBT save/read

--- src/main/java/tterrag/craftingChisel/tile/TileOmniCraftingTable.java
+++ src/main/java/tterrag/craftingChisel/tile/TileOmniCraftingTable.java
@@ -8,6 +8,9 @@ package tterrag.craftingChisel.tile;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import tterrag.craftingChisel.CraftingChisel;
 import tterrag.craftingChisel.util.PacketCraftingTable;
@@ -50,7 +53,7 @@ public class TileOmniCraftingTable extends TileEntity
 	public void writeToNBT(NBTTagCompound tag)
 	{
 		super.writeToNBT(tag);
-		tag.setInteger("id", Block.getIdFromBlock(passedBlock));
+		tag.setInteger("passedID", Block.getIdFromBlock(passedBlock));
 		tag.setInteger("meta", blockMeta);
 		tag.setInteger("marker", marker);
 	}
@@ -59,9 +62,22 @@ public class TileOmniCraftingTable extends TileEntity
 	public void readFromNBT(NBTTagCompound tag)
 	{
 		super.readFromNBT(tag);
-		passedBlock = Block.getBlockById(tag.getInteger("id"));
+		passedBlock = Block.getBlockById(tag.getInteger("passedID"));
  		blockMeta = tag.getInteger("meta");
 		marker = tag.getInteger("marker");
 		hasSent = false;
+	}
+	
+	@Override
+	public Packet getDescriptionPacket(){
+		NBTTagCompound tag = new NBTTagCompound();
+		this.writeToNBT(tag);
+		return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 1, tag);
+	}
+	
+	@Override
+	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt)
+	{
+		this.readFromNBT(pkt.func_148857_g());
 	}
 }
